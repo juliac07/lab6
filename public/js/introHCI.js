88b85d22ca915e9bd305504ae4f53a38203d71c8
juliac07 <juc188@ucsd.edu>
2021-02-11 10:51:06 -0800

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -28,3 +28,13 @@ function addProjectDetails(e) {
 
 	console.log("User clicked on project " + idNumber);
 }
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
+
+$.get("http://URL", callBackFn)
